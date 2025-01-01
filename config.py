1c684436f74141c9b350545e4ad2ee9410a372e6
iqboljon7 <rsfordwyn@gmail.com>
2025-01-01 13:54:03 +0500

    server

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import sqlite3
-import signal
 from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
@@ -11,5 +10,4 @@ load_dotenv()
 
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(storage=MemoryStorage())
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = int(os.getenv("PORT", 5000)) 
--- main.py
+++ main.py
@@ -6,31 +6,6 @@ from states.state import UserStates
 from keyboards.keyboard import phone_number, get_main_menu
 from states.middleware import CheckSubscriptionMiddleware
 from functions import *
-from aiogram.types import Update
-
-
-from aiohttp import web
-
-routes = web.RouteTableDef()
-
-@routes.post("/webhook")
-async def webhook_handler(request: web.Request):
-    try:
-        raw_data = await request.json()
-        update = Update(**raw_data)
-        await dp.feed_update(bot=bot, update=update)
-        return web.Response(text="OK")
-    except Exception as e:
-        print(f"Error in webhook handler: {e}")
-        return web.Response(status=500, text="Internal Server Error")
-
-
-
-async def on_startup(app):
-    await bot.set_webhook(WEBHOOK_URL)
-    logging.info(f"Webhook set at {WEBHOOK_URL}")
-async def on_shutdown(app):
-    await bot.delete_webhook()
 
 
 conn = sqlite3.connect("users.db")
@@ -133,22 +108,9 @@ async def cmd_start(message: types.Message, state: FSMContext):
 async def any_word(msg: types.Message):
     await msg.answer("Siz notog'ri buyruq yubordingiz!")
 
-
 async def main():
-    app = web.Application()
-    app.add_routes(routes)
-    app.on_startup.append(on_startup)
-    app.on_shutdown.append(on_shutdown)
-    logging.basicConfig(level=logging.INFO)
-    logging.info(f"Starting webhook server on port {PORT}")
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, port=PORT)
-    await site.start()
-    await asyncio.Event().wait()
-
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logging.error("Bot stopped!")
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
